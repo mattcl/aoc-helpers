e6e5bd6c6fd1af8625176286156a8ced0fda9961
Matt Chun-Lum <matt@questionable.engineering>
2021-12-30 14:43:59 -0800

    feat: dijkstra path function

--- src/generic/pathing.rs
+++ src/generic/pathing.rs
@@ -33,6 +33,36 @@ where
     }
 }
 
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+pub struct DPNode<T, G>
+where
+    G: Num,
+{
+    id: T,
+    cost: G,
+    path: Vec<T>
+}
+
+impl<T, G> Ord for DPNode<T, G>
+where
+    T: Eq + PartialEq,
+    G: Num + Ord + PartialOrd,
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl<T, G> PartialOrd for DPNode<T, G>
+where
+    T: Eq + PartialEq,
+    G: Num + Ord + PartialOrd,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(other.cost.cmp(&self.cost))
+    }
+}
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct DEdge<T, G>
 where
@@ -140,6 +170,61 @@ where
             let next = DNode {
                 id: edge.id,
                 cost: cost + edge.cost,
+            };
+
+            if next.cost < cost_cache.cache_get(&next.id) {
+                cost_cache.cache_set(&next.id, next.cost);
+                heap.push(next);
+            }
+        }
+    }
+
+    None
+}
+
+pub fn dijkstra_path<T, G, Cache, EdgeFn>(
+    start: T,
+    goal: T,
+    cost_cache: &mut Cache,
+    edges_fn: EdgeFn,
+) -> Option<Vec<T>>
+where
+    T: Eq + PartialEq + Debug + Clone,
+    G: Num + Bounded + Ord + PartialOrd + Clone + Copy,
+    Cache: CostCache<T, Cost = G>,
+    EdgeFn: Fn(&T) -> Vec<DEdge<T, G>>,
+{
+    let mut heap = BinaryHeap::new();
+
+    let start = DPNode {
+        // ideally, we'd copy here, but that might impose too strict of a
+        // restriction on T
+        id: start.clone(),
+        cost: G::zero(),
+        path: vec![start],
+    };
+    cost_cache.cache_set(&start.id, G::zero());
+    heap.push(start);
+
+    while let Some(DPNode {id, cost, path}) = heap.pop() {
+        if id == goal {
+            return Some(path);
+        }
+
+        if cost > cost_cache.cache_get(&id) {
+            continue;
+        }
+
+        for edge in edges_fn(&id) {
+            let mut new_path = path.clone();
+            // again, would prefer copy but that would require imposing copy on
+            // T, which is not something I want to do
+            new_path.push(edge.id.clone());
+
+            let next = DPNode {
+                id: edge.id,
+                cost: cost + edge.cost,
+                path: new_path,
             };
 
             if next.cost < cost_cache.cache_get(&next.id) {
