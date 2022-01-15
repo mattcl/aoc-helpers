3f91c205b73001dc8c81f1e090201a3fcefdbdb0
Matt Chun-Lum <matt@questionable.engineering>
2022-01-15 12:16:40 -0800

    feat: initial docs branch

--- src/generic/prelude.rs
+++
@@ -1,9 +0,0 @@
-//! The aoc_helpers generic prelude imports various traits that enable
-//! functionality for various generic constructs like [Grid](super::grid::Grid)
-//! ```
-//! use aoc_helpers::generic::prelude::*;
-//! ```
-pub use super::grid::GridLike;
-pub use super::grid::Rectangular;
-pub use super::grid::Scalable;
-pub use super::pathing::CostCache;
